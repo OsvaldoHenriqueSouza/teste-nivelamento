0024be1840f37b9733242e6d95421ebc5ac6cf32
Osvaldo Henrique dos Santos Souza <osvaldosouza_99@hotmail.com>
2023-04-13 23:45:56 -0300

    ajusta exercicio 05

--- exercicio-05/script.js
+++ exercicio-05/script.js
@@ -6,18 +6,6 @@ const parseNumber = input => Number(input);
 
 const calculaAreaCirculo = raio => raio * raio * Math.PI.toFixed(5);
 
-const verificaInput = elements => {
-  const regex = /[0-9]/i;
-  elements.forEach((item) => {
-      item.addEventListener("keyup", ({ target }) => {
-        const validacao = regex.test(target.value);
-        if (!validacao) {
-          target.value = "";
-        }
-      });
-  });
-}
-
 verificaInput(input);
 
 form.addEventListener("submit", (event) => {
