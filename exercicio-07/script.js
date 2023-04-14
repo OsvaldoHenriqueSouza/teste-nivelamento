23b45ede40b9871030ab320987878aa541e93861
Osvaldo Henrique dos Santos Souza <osvaldosouza_99@hotmail.com>
2023-04-13 23:48:26 -0300

    ajuste exercicio 07

--- exercicio-07/script.js
+++ exercicio-07/script.js
@@ -6,18 +6,6 @@ const parseNumber = input => Number(input);
 
 const calculaMediaConsumo = (distancia, consumo) => distancia / consumo;
 
-const verificaInput = elements => {
-  const regex = /[0-9]/i;
-  elements.forEach((item) => {
-      item.addEventListener("keyup", ({ target }) => {
-        const validacao = regex.test(target.value);
-        if (!validacao) {
-          target.value = "";
-        }
-      });
-  });
-}
-
 verificaInput(input);
 
 form.addEventListener("submit", (event) => {
